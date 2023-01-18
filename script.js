d70dee369906bb8a9a932df0d421d54d66f96fc4
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2023-01-18 11:18:31 -0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict'
- 
+ let readMoreButton = document.getElementsByClassName(".read-more")
  let hiddenBlockQuotes = document.getElementById("more-content")
 
  hiddenBlockQuotes.style.display="none"
+ 
+readMoreButton.addEventListener('click',()=>{
+
+    hiddenBlockQuotes.style.display="block"
+
+})
