476fa21b0cca4205d75e8c32daa84426e63782e6
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2023-01-18 15:01:19 -0700

    update

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 'use strict'
  const readMoreButton = document.querySelector(".read-more");
- const hiddenBlockQuotes = document.querySelector("#more-content");
+ const hiddenBlockQuotes = document.querySelector(".more-content");
 
 
 //  hiddenBlockQuotes.style.display="none";
 
  readMoreButton.addEventListener("click",function(){
 console.log("button clicked");
-if(hiddenBlockQuotes.style.display="none")   
-hiddenBlockQuotes.style.display="block";
-else if(hiddenBlockQuotes.style.display="block"){
-    hiddenBlockQuotes.style.display="none";
-}
+if(hiddenBlockQuotes.classList.contains('more-content')){
+    hiddenBlockQuotes.classList.remove('more-content');
+} 
+ else{
+    hiddenBlockQuotes.classList.add('more-content');
+ }
+console.log("click");
 })
-
