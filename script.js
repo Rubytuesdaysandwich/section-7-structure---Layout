24934371d7b0a7b9c9e6dd93f3911ad73e1ccd5d
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2023-01-18 13:26:23 -0700

    update working event

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict'
- let readMoreButton = document.getElementsByClassName(".read-more")
- let hiddenBlockQuotes = document.getElementById("more-content")
+ const readMoreButton = document.querySelector(".read-more");
+ const hiddenBlockQuotes = document.querySelector("#more-content");
 
- hiddenBlockQuotes.style.display="none"
- 
-readMoreButton.addEventListener('click',()=>{
 
-    hiddenBlockQuotes.style.display="block"
+//  hiddenBlockQuotes.style.display="none";
 
+ readMoreButton.addEventListener("click",function(){
+console.log("button clicked");
+if(hiddenBlockQuotes.style.display="none")   
+hiddenBlockQuotes.style.display="block";
+else{
+    hiddenBlockQuotes.style.display="none";
+}
 })
+
