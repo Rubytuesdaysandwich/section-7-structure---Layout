a770f743a6ec09f4da58bedc20460cbd6cc994cb
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2023-01-18 15:46:37 -0700

    completed

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
  const hiddenBlockQuotes = document.querySelector(".more-content");
 
 
-//  hiddenBlockQuotes.style.display="none";
 
  readMoreButton.addEventListener("click",function(){
 console.log("button clicked");
@@ -15,3 +14,4 @@ if(hiddenBlockQuotes.classList.contains('more-content')){
  }
 console.log("click");
 })
+//This script is used to show and hide the more-content section.
